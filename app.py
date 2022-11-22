8dba506b2f7ec37522dc52a9611a964df4ce51c6
tinechitando <68731529+tinechitando@users.noreply.github.com>
2022-11-22 13:49:22 +0200

    project

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+import os
+from werkzeug.utils import secure_filename
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+UPLOAD_FOLDER = './flask app/assets/images'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# Create Database if it doesnt exist
+
+app = Flask(__name__,static_url_path='/assets',
+            static_folder='./flask app/assets', 
+            template_folder='./flask app')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def root():
+   return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+   return render_template('index.html')
+
+
+@app.route('/news.html')
+def news():
+   return render_template('news.html')
+
+@app.route('/about.html')
+def about():
+   return render_template('about.html')
+
+
+@app.route('/upload.html')
+def upload():
+   return render_template('upload.html')
+
+@app.route('/upload_chest.html')
+def upload_chest():
+   return render_template('upload_chest.html')
+
+
+@app.route('/uploaded_chest', methods = ['POST', 'GET'])
+def uploaded_chest():
+   if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_chest.jpg'))
+
+   resnet_chest = load_model('Notebook/resnet_chest_final.h5')
+   
+   image = cv2.imread('./flask app/assets/images/upload_chest.jpg') # read file 
+   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
+   image = cv2.resize(image,(224,224))
+   image = np.array(image) / 255
+   image = np.expand_dims(image, axis=0)
+   
+   resnet_pred = resnet_chest.predict(image)
+   probability = resnet_pred[0]
+   print("Resnet Predictions:")
+   if probability[0] > 0.5:
+      resnet_chest_pred = str('%.2f' % (probability[0]*100) + '% LUNG INFECTION') 
+   else:
+      resnet_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% HEALTHY')
+   print(resnet_chest_pred)
+
+
+   return render_template('results_chest.html',resnet_chest_pred=resnet_chest_pred)
+
+
+if __name__ == '__main__':
+   app.secret_key = ".."
+   app.run()
